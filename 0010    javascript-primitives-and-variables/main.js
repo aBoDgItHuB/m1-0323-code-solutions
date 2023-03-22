49d10f5dc495d25bdb41a1ca71aadd73f447e566
young.kim@usres.com <young.kim@usres.com>
2023-03-22 00:19:59 +0000

    Completed - javascript-primitives-and-variables.

---
+++ 0010    javascript-primitives-and-variables/main.js
@@ -0,0 +1,28 @@
+// #9.
+const fullName = 'Young Jae KIM';
+const isCool = true;
+const totalPets = 0;
+
+// #11.
+console.log('value of fullName: ' + fullName);
+console.log('value of isCool: ' + isCool);
+console.log('value of totalPets: ' + totalPets);
+
+// #12.
+console.log('typeof fullName: ' + typeof fullName);
+console.log('typeof isCool: ' + typeof isCool);
+console.log('typeof totalPets: ' + typeof totalPets);
+
+// #14.
+const unknown = null;
+// #14.1
+console.log('value of unknown: ' + unknown);
+// #14.2
+console.log('typeof unknown: ' + typeof unknown);
+
+// #16.
+let never;
+// #16.1.
+console.log('value of never: ' + never);
+// #16.2
+console.log('typeof never: ' + typeof never);
